0dd547a24f3dd5cdedc5263506984933deec5206
Lucas-ulerich <lucas.ulerichsouza@gmail.com>
2023-01-24 10:30:02 -0300

    Mudando arquivos de repositorio

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,40 @@
+<template>
+  <box>
+    <div class="columns">
+      <div class="column is-7">
+        {{ tarefa.descricao || 'Tarefa sem descrição' }}
+      </div>
+      <div class="column">
+        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
+      </div>
+    </div>
+  </box>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import Cronometro from './Cronometro.vue'
+import ITarefa from '../Interfaces/ITarefa'
+import box from './box.vue'
+
+
+export default defineComponent({
+  name: 'Tarefa-tracker',
+  components: {
+    Cronometro,
+    box
+  },
+  props: {
+    tarefa: {
+      type: Object as PropType<ITarefa>,
+      required: true
+    }
+  },
+})
+</script>
+
+<style scoped>
+.box {
+  background: #FAF0CA;
+}
+</style>
